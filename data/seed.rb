da48053ad59ae0075ab929e76191bc8c9c6d4455
aviflombaum <avi@flombaum.com>
2018-05-08 07:56:33 -0400

    Wow

--- data/seed.rb
+++ data/seed.rb
@@ -11,23 +11,37 @@ q2 = Question.new(:id => "Q2", :text => "Is the event important?", :answers => {
 })
 
 q3 = Question.new(:id => "Q3", :text => "Think you'll have fun?", :answers => {
-    "Yes" => "A2",
-    "Maybe" => "A2",
-    "No" => "Q4"
+    "Yes" => "A3",
+    "Maybe" => "Q5",
+    "No" => "Q2"
 })
 
 q4 = Question.new(:id => "Q4", :text => "Is this the only time you can go?", :answers => {
+    "Yes" => "A2",
+    "No" => "Q6"
+})
+
+q5 = Question.new(:id => "Q5", :text => "Will you be with friends?", :answers => {
+    "Yes" => "A2",
+    "No" => "Q6"
+})
+
+q6 = Question.new(:id => "Q6", :text => "Will there be free food?", :answers => {
     "Yes" => "A2",
     "No" => "A1"
 })
 
 a1 = Answer.new(:id => "A1", :text => "Stay In")
 a2 = Answer.new(:id => "A2", :text => "Go Out")
+a2 = Answer.new(:id => "A3", :text => "Ya, go out, have a ball!")
 
 quiz.add_question(q1)
 quiz.add_question(q2)
 quiz.add_question(q3)
 quiz.add_question(q4)
+quiz.add_question(q5)
+quiz.add_question(q6)
 
 quiz.add_answer(a1)
 quiz.add_answer(a2)
+quiz.add_answer(a3)
